4bc78ce934afac1ffbe859b4543c6cf3a6821925
horetha <horeth@deadlinesolutions.com>
2020-04-09 10:05:34 -0400

    Propogate files into the folders, update 02

---
+++ propogate.ps1
@@ -0,0 +1,13 @@
+﻿# copy these items into the subdirectories and remove from base
+$files = @('index.html', 'style.css');
+Get-ChildItem -Directory | ForEach {
+    $dir = $_.FullName
+    
+    foreach($f in $files) {
+        Copy-Item -Force ($(Get-Item -Path $f).FullName) -Destination $_.FullName
+    }
+}
+
+foreach($f in $files) {
+    Remove-Item $f
+}
